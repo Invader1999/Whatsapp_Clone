cad32506adc3baf19ecf402f7dc6af6578a13ce9
Hemanth <hemanthreddy056@gmail.com>
2024-04-22 22:41:14 +0530

    Initial Commit

---
+++ Whatsapp_Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Whatsapp_Clone
+//
+//  Created by Kareddy Hemanth Reddy on 22/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Whatsapp_Clone/Whatsapp_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whatsapp_CloneApp.swift
+//  Whatsapp_Clone
+//
+//  Created by Kareddy Hemanth Reddy on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Whatsapp_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
