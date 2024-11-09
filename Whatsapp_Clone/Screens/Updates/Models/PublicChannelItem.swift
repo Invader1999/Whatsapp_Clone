251f98ae2e3ce45b616112b3d4a8f51c12c978e0
Hemanth <hemanthreddy056@gmail.com>
2024-11-09 19:46:14 +0530

    made changes in updates tab

---
+++ Whatsapp_Clone/Screens/Updates/Models/PublicChannelItem.swift
@@ -0,0 +1,18 @@
+//
+//  PublicChannelItem.swift
+//  Whatsapp_Clone
+//
+//  Created by Kareddy Hemanth Reddy on 06/11/24.
+//
+
+import SwiftUI
+
+struct PublicChannelItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PublicChannelItem()
+}
